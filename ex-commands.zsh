2d9e8b52df9d857cd052ecf180644aaf1b7a02cb
gammafn <GammaFunction@vivaldi.net>
2021-02-01 15:32:29 -0600

    vared doesn't work, read-from-minibuffer does

--- ex-commands.zsh
+++ ex-commands.zsh
@@ -3,7 +3,7 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 [[ $PMSPEC = *f* ]] || fpath+=("${0:h}/functions")
-autoload -Uz ex-command _zvm_colon_cmd
+autoload -Uz read-from-minibuffer ex-command _zvm_ex_command
 # }}}
 zstyle ':completion:zvm-cmd:*' completer _zvm_ex_command
 zle -N ex-command
